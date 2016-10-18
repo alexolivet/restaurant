0bd8dbb22584727f20248e9a9b04675c1e465fcd
Subash Pradhan <suprowilde5@gmail.com>
2016-10-18 13:53:08 +0200

    made little more of resposive

--- js/home.js
+++ js/home.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 	var header = $('body');
 
-	var backgrounds = new Array('url(images/img-7.jpg)', 'url(images/img-8.jpg)', 'url(images/img-9.jpg)',
-	    'url(images/img-10.jpg)', 'url(images/img-11.jpg)');
+	var backgrounds = ['url(images/img-7.jpg)', 'url(images/img-8.jpg)', 'url(images/img-9.jpg)',
+	    'url(images/img-10.jpg)', 'url(images/img-11.jpg)'];
 	    
 	var current = 0;
 
@@ -15,12 +15,22 @@ $(document).ready(function(){
 	setInterval(nextBackground, 3000);
 
 	header.css('background-image', backgrounds[0]);
+	
+	if ($(window).width() < 1024) {
+		var backgrounds = ['url(images/img-12.jpg)'];
+		function nextBackground() {
+	    current++;
+	    current = current % backgrounds.length;
+	    header.css('background-image', backgrounds);
+	}
+	nextBackground();
 
-
-
-
-
-
+	header.css('background-image', backgrounds);
+	}
 });
+
+
+
+
 
 
