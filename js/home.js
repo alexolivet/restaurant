8b178f8130acc245c72b5771b8149b8756317997
Subash Pradhan <suprowilde5@gmail.com>
2016-10-20 16:26:51 +0200

    made few changes in js file

--- js/home.js
+++ js/home.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
 	var header = $('body');
 
 	var backgrounds = ['url(images/img-7.jpg)', 'url(images/img-8.jpg)', 'url(images/img-9.jpg)',
-	    'url(images/img-10.jpg)', 'url(images/img-11.jpg)'];
-	    
+	'url(images/img-10.jpg)', 'url(images/img-11.jpg)'];
+
 	var current = 0;
 
 	function nextBackground() {
-	    current++;
-	    current = current % backgrounds.length;
-	    header.css('background-image', backgrounds[current]);
+		current++;
+		current = current % backgrounds.length;
+		header.css('background-image', backgrounds[current]);
 	}
 	setInterval(nextBackground, 3000);
 
@@ -18,14 +18,7 @@ $(document).ready(function(){
 	
 	if ($(window).width() < 1024) {
 		var backgrounds = ['url(images/img-13.jpg)'];
-		function nextBackground() {
-	    current++;
-	    current = current % backgrounds.length;
-	    header.css('background-image',backgrounds);
-	}
-	nextBackground();
-
-	header.css('background-image', backgrounds);
+		header.css('background-image',backgrounds);
 	}
 });
 
